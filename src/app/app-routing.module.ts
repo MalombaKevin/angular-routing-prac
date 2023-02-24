187059365ef4438069962b70717c0cb4349929d2
Kevin Malomba <malombakevin@gmail.com>
2023-02-24 08:56:56 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products/products.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'products', component:ProductsComponent},
   {path: 'products/title/:id', component:ProductComponent},
+  {path: '**', component:NotFoundComponent},
   
 ];
 
